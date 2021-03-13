23392e1a880e3569577b8b8a82699fc2a992bb37
Markus <66058642+mhovd@users.noreply.github.com>
2021-03-14 00:21:29 +0100

    Update NPrepScript.R

--- inst/report/NPrepScript.R
+++ inst/report/NPrepScript.R
@@ -6,6 +6,16 @@ cat(commandArgs(), "\n")
 #icen <- commandArgs()[7]
 #parallel <- as.logical(commandArgs()[8])
 
+# Find the right argument
+cat("Argument 1: ", commandArgs()[1], "\n")
+cat("Argument 2: ", commandArgs()[2], "\n")
+cat("Argument 3: ", commandArgs()[3], "\n")
+cat("Argument 4: ", commandArgs()[4], "\n")
+cat("Argument 5: ", commandArgs()[5], "\n")
+cat("Argument 6: ", commandArgs()[6], "\n")
+cat("Argument 7: ", commandArgs()[7], "\n")
+cat("Argument 8: ", commandArgs()[8], "\n")
+
 # New argument indices
 wd <- commandArgs()[1]
 icen <- commandArgs()[2]
