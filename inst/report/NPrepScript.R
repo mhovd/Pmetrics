6c84d249027559012001b593c9acc02ce5d28ac5
Markus <66058642+mhovd@users.noreply.github.com>
2021-03-13 23:57:05 +0100

    Update NPrepScript.R

--- inst/report/NPrepScript.R
+++ inst/report/NPrepScript.R
@@ -1,9 +1,9 @@
-cat("If I appear, I am the culprit file! \n")
 require(Pmetrics)
+cat(commandArgs(), "\n")
 wd <- commandArgs()[6]
 icen <- commandArgs()[7]
 parallel <- as.logical(commandArgs()[8])
-cat("NPrepScript thinks this is the wd:\n", wd)
+cat("NPrepScript thinks this is the wd:\n", wd, "\n")
 setwd(wd)
 PMreport(wd,icen=icen,type="NPAG",parallel=parallel)
 
