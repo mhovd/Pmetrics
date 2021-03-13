fac779959a9606823085b2ea2fe491c9631976d3
Markus <66058642+mhovd@users.noreply.github.com>
2021-03-13 23:31:08 +0100

    Finding the culprit

--- inst/report/NPrepScript.R
+++ inst/report/NPrepScript.R
@@ -1,9 +1,10 @@
-require(Pmetrics)
-wd <- commandArgs()[6]
-icen <- commandArgs()[7]
-parallel <- as.logical(commandArgs()[8])
-setwd(wd)
-PMreport(wd,icen=icen,type="NPAG",parallel=parallel)
+cat("If I appear, I am the culprit file! \n")
+#require(Pmetrics)
+#wd <- commandArgs()[6]
+#icen <- commandArgs()[7]
+#parallel <- as.logical(commandArgs()[8])
+#setwd(wd)
+#PMreport(wd,icen=icen,type="NPAG",parallel=parallel)
 
 
 
